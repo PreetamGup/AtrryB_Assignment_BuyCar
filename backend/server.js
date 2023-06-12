67383ccddb0d61328545af9e916896d8fbe2378d
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2023-06-12 09:32:41 +0530

    added morgan

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const dotenv = require("dotenv")
 const cors = require("cors")
 const connectDb = require("./config/connectDB")
+const logger = require("morgan")
 
 const app= express();
 
@@ -12,6 +13,7 @@ dotenv.config();
 //connect to Db
 connectDb()
 
+app.use(logger('dev'))
 app.use(cors())
 app.use(express.json())
 
@@ -19,7 +21,8 @@ app.use(express.json())
 app.use('/api/user', require('./routes/userRoutes'));
 app.use("/api/car", require("./routes/carRoutes") )
 
+const Port = process.env.PORT || 5050
 
-app.listen('5050',()=>{
-    console.log("Server is listening on Port 5050")
+app.listen(Port,()=>{
+    console.log(`Server is listening on Port ${Port}`)
 })
